1958a7695ff98f650b7884dd12f693762066c908
cptShell <repozheka@gmail.com>
2022-05-11 22:37:00 +0300

    PMA-4: type relocation

--- src/services/auth-api/auth-api.service.ts
+++ src/services/auth-api/auth-api.service.ts
@@ -1,20 +1,15 @@
 import { AuthApiPath, ContentType, HttpMethod } from '~/common/enums/enums';
-import { SignInUserDto, SignUpUserDto } from '~/common/types/types';
+import {
+  SignInResponseDto,
+  SignInUserDto,
+  SignUpResponseDto,
+  SignUpUserDto,
+} from '~/common/types/types';
 import { Http } from '~/services/http/http.service';
 
 type Constructor = {
   http: Http;
   apiPrefix: string;
-};
-
-type SingInResponseDto = {
-  token: string;
-};
-
-type SignUpResponseDto = {
-  id: string;
-  name: string;
-  login: string;
 };
 
 export class AuthApi {
@@ -26,7 +21,7 @@ export class AuthApi {
     this.#apiPrefix = apiPrefix;
   }
 
-  signIn(payload: SignInUserDto): Promise<SingInResponseDto> {
+  signIn(payload: SignInUserDto): Promise<SignInResponseDto> {
     return this.#http.load(`${this.#apiPrefix}${AuthApiPath.SIGN_IN}`, {
       method: HttpMethod.POST,
       hasAuth: false,
