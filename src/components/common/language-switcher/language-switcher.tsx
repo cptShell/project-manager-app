6c388f591f113caacff6e3a1a25ca49620657935
Nazeroth <danieldefoe22@gmail.com>
2022-05-11 13:10:56 +0300

    PMA-10: refactor buttons to ul with li inside, li made as separated component

--- src/components/common/language-switcher/language-switcher.tsx
+++ src/components/common/language-switcher/language-switcher.tsx
@@ -1,16 +1,18 @@
 import { FC } from 'react';
 import { AppLanguage } from '~/common/enums/enums';
 import { useAppTranslation } from '~/hooks/hooks';
+import { LanguageSwitcherItem } from './components/language-switcher-item';
 
 export const LanguageSwitcher: FC = () => {
 
   const { handleLanguageChange } = useAppTranslation();
 
   return (
-    <div>
-      <button onClick={():void => handleLanguageChange(AppLanguage.EN)}>EN</button>
-      <button onClick={():void => handleLanguageChange(AppLanguage.RU)}>RU</button>
-    </div>
+    <ul>
+      {Object.values(AppLanguage).map((language, index) =>  (
+        <LanguageSwitcherItem key={index} label={language} onLanguageChange={handleLanguageChange} />
+      ))}
+    </ul>
   );
 };
 
