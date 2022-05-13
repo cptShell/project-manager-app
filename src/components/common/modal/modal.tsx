7c7736fc88749ac4d57b4699ad89ada71411dc0e
AlexRoman <mirmaker181@gmail.com>
2022-05-13 19:33:55 +0300

    PMA-13: refactor according to PR suggestions 6

--- src/components/common/modal/modal.tsx
+++ src/components/common/modal/modal.tsx
@@ -1,4 +1,4 @@
-import { FC, MouseEventHandler } from 'react';
+import { FC } from 'react';
 import { createPortal } from 'react-dom';
 
 import styles from './styles.module.scss';
@@ -6,15 +6,21 @@ import styles from './styles.module.scss';
 type Props = {
   children: JSX.Element,
   isOpen: boolean,
-  onClose: MouseEventHandler,
+  onClose: () => void,
 };
 
 export const Modal: FC<Props> = ({ children, isOpen, onClose }) => {
+  const handleClose = (e: React.MouseEvent): void => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+  
   if (!isOpen) {
     return null;
   }
   return createPortal(
-    <div className={styles.wrapper} onClick={onClose}>
+    <div className={styles.wrapper} onClick={handleClose}>
       <div className={styles.innerWrapper}>
         {children}
       </div>
