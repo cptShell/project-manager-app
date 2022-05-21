df7fc822d9c094bc298aef946f3c16f9ac95366c
Nazeroth <danieldefoe22@gmail.com>
2022-05-21 11:47:17 +0300

    PMA-5: add board creation

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -1,6 +1,7 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AppRoute } from '~/common/enums/enums';
+import { BoardCreatingForm } from '~/components/main/components/board-creating-form';
 import { useAppDispatch } from '~/hooks/hooks';
 import { auth as authActions } from '~/store/actions';
 import { LanguageSwitcher } from '../language-switcher/language-switcher';
@@ -8,22 +9,24 @@ import { LanguageSwitcher } from '../language-switcher/language-switcher';
 export const Header: FC = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleSignOut = (): void => {
     dispatch(authActions.signOut());
   };
 
   const handleEditUser = (): void => {
-    navigate(AppRoute.EDIT_USER);
+    navigate(AppRoute.PROFILE);
   };
 
   const handleCreateBoard = (): void => {
-    //TODO: complete logic after adding board route
+    setIsOpen(true);
   };
 
   return (
     <div>
       <LanguageSwitcher />
+      <BoardCreatingForm isOpen={isOpen} setIsOpen={setIsOpen} />
       <button onClick={handleCreateBoard}>Create board</button>
       <button onClick={handleEditUser}>Edit user</button>
       <button onClick={handleSignOut}>Sign Out</button>
---
+++ src/components/main/components/board-creating-form.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import { board as boardActions } from '~/store/actions';
+import { joiResolver } from '@hookform/resolvers/joi';
+import { createBoard } from '~/validation-schemas/validation-schemas';
+import { InputName } from '~/common/enums/enums';
+import { TextInput } from '~/components/common/common';
+import { CreateBoardDto } from '~/common/types/types';
+import { useAppDispatch } from '~/hooks/hooks';
+
+type Props = {
+  isOpen: boolean;
+  setIsOpen: (arg: boolean) => void;
+};
+
+export const BoardCreatingForm: FC<Props> = ({ isOpen, setIsOpen }) => {
+  const { register, handleSubmit, formState, reset } = useForm<CreateBoardDto>({
+    resolver: joiResolver(createBoard),
+  });
+  const { title: titleError } = formState.errors;
+  const dispatch = useAppDispatch();
+
+  const handleCreateForm = ({ title }: CreateBoardDto): void => {
+    dispatch(boardActions.create({ title }));
+    reset();
+    setIsOpen(false);
+  };
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <form onSubmit={handleSubmit(handleCreateForm)}>
+      <h2>Board creating form</h2>
+      <TextInput
+        formRegisterValues={register(InputName.TITLE)}
+        errorMessage={titleError?.message}
+      />
+      <button>Create board</button>
+    </form>
+  );
+};
