f2ab4a7bbcec936291d9168c5e40797fa7428ae1
Nazeroth <danieldefoe22@gmail.com>
2022-05-27 00:09:50 +0300

    PMA-39: add 404 page styling

--- src/components/not-found-page/not-found-page.tsx
+++ src/components/not-found-page/not-found-page.tsx
@@ -1,6 +1,30 @@
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AppRoute } from '~/common/enums/enums';
+import styles from './styles.module.scss';
 
-export const NotFound:FC = () => {
-  return <div>NotFound</div>;
+export const NotFound: FC = () => {
+  const navigate = useNavigate();
+
+  const handleReturn = (): void => {
+    navigate(AppRoute.MAIN);
+  };
+
+  return (
+    <div>
+      <iframe
+        className={styles['video-player']}
+        src={
+          'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&disablekb=1&fs=0&loop=1&modestbranding=1&playsinline=1&iv_load_policy=3&mute=1'
+        }
+      ></iframe>
+      <div className={styles['navigation-wrapper']}>
+        <div className={styles['navigation-container']}>
+          <div className={styles['navigation-button']} onClick={handleReturn}>
+            Probably you are lost, click on Rick to return on Main page
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
-
