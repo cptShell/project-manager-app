a59f6998f7c27efc25e7edaa756a049fc249f7b8
AlexRoman <mirmaker181@gmail.com>
2022-06-09 01:52:21 +0300

    PMA-67: fix input styles

--- src/components/common/input/text-input.tsx
+++ src/components/common/input/text-input.tsx
@@ -1,8 +1,10 @@
+import clsx from 'clsx';
 import { FC } from 'react';
 import { UseFormRegisterReturn } from 'react-hook-form';
 import { InputType } from '~/common/enums/enums';
 import { AppLocalizationKey } from '~/common/types/types';
 import { FormattedMessage } from '../common';
+import styles from './styles.module.scss';
 
 type Props = {
   title: AppLocalizationKey;
@@ -19,16 +21,22 @@ export const TextInput: FC<Props> = ({
   className,
   disabled,
 }) => {
+  const isError = Boolean(errorMessage);
+  const inputStyle = clsx(styles.input, {
+    [styles['input-error']]: isError,
+  });
   return (
     <label className={className}>
       <FormattedMessage as="span" message={title} />
       <input
+        className={inputStyle}
         type={InputType.TEXT}
         {...formRegisterValues}
         disabled={disabled}
       />
-      {Boolean(errorMessage) && (
+      {isError && (
         <FormattedMessage
+          className={styles.error}
           as="span"
           message={errorMessage as AppLocalizationKey}
         />
