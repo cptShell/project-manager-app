e079680a8e3b4a69465739b72df455b8e70a0d6a
cptShell <repozheka@gmail.com>
2022-06-20 17:34:41 +0300

    PMA-69: fix id

--- src/components/board/components/task-link/task-link.tsx
+++ src/components/board/components/task-link/task-link.tsx
@@ -55,7 +55,7 @@ export const TaskLink: FC<Props> = ({
 
   return (
     <Draggable
-      draggableId={`${taskIndex}-${title}`}
+      draggableId={`${data.id}`}
       index={taskIndex}
       isDragDisabled={isDragging}
     >
